cf2c82d7d28c8df3d344f2b1d49f2c2cffdac030
Viplove Vaishnava <160388904+viplovevaishnava@users.noreply.github.com>
2026-08-29 05:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Testing the function
+console.log(getKeys(student));
